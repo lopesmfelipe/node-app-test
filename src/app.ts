dce8938a1f9c7d114b7852d0821e99309ab2da5a
Felipe Lopes <felipemlopes0009@gmail.com>
2024-09-03 01:50:47 -0400

    Handling undefined routes

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,19 @@ import wRouter from "./routes/wRoutes.js";
 
 const app = express();
 
+// MIDDLEWARES
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+// ROUTES
 app.use("/whatever", wRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 export default app;
