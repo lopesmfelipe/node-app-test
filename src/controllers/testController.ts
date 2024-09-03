cef368eeb2f23e71bd0158a4eb59ef2091820d38
Felipe Lopes <felipemlopes0009@gmail.com>
2024-09-03 18:30:49 -0400

    catchAsync function in testController

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,14 +1,20 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, response } from "express";
+import { request } from "http";
 
-export const getTest = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const message = "THIS IS THE MESSAGE";
-
-  res.status(200).json({
-    status: "success",
-    message,
-  });
+const catchAsync = (fn: any) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
 };
+
+// GET TEST
+export const getTest = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const message = "THIS IS THE MESSAGE";
+
+    res.status(200).json({
+      status: "success",
+      message,
+    });
+  }
+);
