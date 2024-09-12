36c301b24c0e9a648ebddd2b6dac418fe7110b73
Felipe Lopes <felipemlopes0009@gmail.com>
2024-09-12 12:33:10 -0400

    Implementing rate limiting

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 import wRouter from "./routes/whateverRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import AppError from "./utils/appError.js";
@@ -9,8 +10,17 @@ import GlobalErrorHandler from "./controllers/globalErrorHandler.js";
 const app = express();
 
 // MIDDLEWARES
+app.use(morgan("dev")); 
+
+// Limit the number of requests from the same IP per hour
+const limiter = rateLimit({
+  max: 2, // max 100 requests
+  windowMs: 60 * 60 * 1000, // time limit of 1 hour
+  message: 'Too many requests from this IP, please try again in 1 hour'
+});
+app.use('/', limiter);
+
 app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
 
 // ROUTES
